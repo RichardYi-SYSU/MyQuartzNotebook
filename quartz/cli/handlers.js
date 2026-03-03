4d0c358f9b19856d8b6de93aa5201d420a168bf8
RichardYi-SYSU-Mac <1641687849@qq.com>
2026-03-03 09:50:39 +0800

    Update quartz config and CLI log colors

--- quartz/cli/handlers.js
+++ quartz/cli/handlers.js
@@ -395,7 +395,7 @@ export async function handleBuild(argv) {
           status >= 200 && status < 300
             ? styleText("green", `[${status}]`)
             : styleText("red", `[${status}]`)
-        console.log(statusString + styleText("grey", ` ${argv.baseDir}${req.url}`))
+        console.log(statusString + styleText("gray", ` ${argv.baseDir}${req.url}`))
         release()
       }
 
@@ -405,8 +405,8 @@ export async function handleBuild(argv) {
           Location: newFp,
         })
         console.log(
-          styleText("yellow", "[302]") +
-            styleText("grey", ` ${argv.baseDir}${req.url} -> ${newFp}`),
+            styleText("yellow", "[302]") +
+              styleText("gray", ` ${argv.baseDir}${req.url} -> ${newFp}`),
         )
         res.end()
       }
@@ -482,7 +482,7 @@ export async function handleBuild(argv) {
       .on("change", () => build(clientRefresh))
       .on("unlink", () => build(clientRefresh))
 
-    console.log(styleText("grey", "hint: exit with ctrl+c"))
+    console.log(styleText("gray", "hint: exit with ctrl+c"))
   }
 }
 
